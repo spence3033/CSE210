afd9aff4a13d92ebb8ac417aea0c9c17f7e3a6c2
Spencer Birch <bir18006@byui.edu>
2022-01-15 12:00:36 -0700

    I finished the TicTacToe

--- tictactoe.py
+++ tictactoe.py
@@ -17,8 +17,7 @@ def main():
         return: None
     '''
     # assign/get the first player
-    person1 = input("What is the first player's name? ")
-
+    player = 1
     # create a board
     board = [blank, blank, blank, 
              blank, blank, blank, 
@@ -29,21 +28,21 @@ def main():
     no_tie = True
 
     while no_winner == True and no_tie == True:
-        for item in board:
-            is_draw(board)
-            is_winner(board)
 
         # display the board
         display_board(board)
         # allow the player to make a move
-        make_move(person1, board)
         # pick the next player
+        player = make_move(player, board)
 
-        pass
+        no_winner = not is_winner(board)
+        no_tie = not is_draw(board)
+
     # display the final board
+    display_board(board)
 
     # show message for winner and thanks for playing
-    pass
+    print(f'Congradulations! Game over!')
 
 def create_board():
     ''' Creates a list that holds the spots on the board
@@ -65,11 +64,43 @@ def display_board(board):
 
 def is_draw(board):
     ''' return: True if board is a draw, False if board is still playable '''
-    pass
+    for item in board:
+        if item == ' ':
+            return False
+    return True
 
 def is_winner(board):
     ''' return: True if someone won, False if there is no winner '''
-    pass
+    list_Xs = []
+    list_Os = []
+    number = 0
+    item = board[0]
+    if item == 'X' or item == 'O':
+        if board[1] == item and board[2] == item:
+            return True
+        if board[3] == item and board[6] == item:
+            return True
+        if board[4] == item and board[8] == item:
+            return True
+    item = board[1]
+    if item == 'X' or item == 'O':
+        if board[4] == item and board[7] == item:
+            return True
+    item = board[2]
+    if item == 'X' or item == 'O':
+        if board[5] == item and board[8] == item:
+            return True
+    item = board[3]
+    if item == 'X' or item == 'O':
+        if board[4] == item and board[5] == item:
+            return True
+    item = board[6]
+    if item == 'X' or item == 'O':
+        if board[7] == item and board[8] == item:
+            return True
+        if board[4] == item and board[2] == item:
+            return True
+    return False
 
 def make_move(player, board):
     ''' Prompts player to select a square to play
@@ -78,15 +109,22 @@ def make_move(player, board):
     '''
     should_move = True
     while should_move == True:
-        move = input('Please choose 1-9 for where you want to place and "X": ')
-        if board[move-1] == " ":
-            board[move-1] = X
-            should_move = False
-    pass      
-
-def next_player(current):
-    ''' return: x if current is o, otherwise x '''
-    pass
+        move = int(input('Please choose 1-9 for where you want to place and "X": '))
+        move = move - 1
+        if board[move] == " ":
+            if player == 1:
+                board[move] = X
+                should_move = False
+            if player == 2:
+                board[move] = O
+                should_move = False
+        else:
+            print(f'the board has an "{board[move]}" there. Try again.')
+    
+    if player == 1:
+        return 2
+    elif player == 2:
+        return 1
 
 # run main if this has been called from the command line
 if __name__ == "__main__":
